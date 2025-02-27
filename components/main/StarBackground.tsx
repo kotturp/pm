ac0ecffb43dc988b5219fb78d0ba3e602480f5fc
kotturp <prasannakttur@gmail.com>
2025-02-27 00:25:42 -0600

    Update StarBackground.tsx

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -3,11 +3,11 @@
 import React, { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial,} from "@react-three/drei";
-// @ts-ignore
+
 import * as random from "maath/random/dist/maath-random.esm";
 
 const StarBackground = (props: any) => {
-  const ref = useRef<any>(null); // ✅ Properly initialize useRef
+  const ref = useRef<THREE.Points | null>(null);
 
   const [sphere] = useState(() => {
     const positions = new Float32Array(5000 * 3); // ✅ Fix array size
